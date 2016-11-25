6b759d3f4833854ab4e6cd3dfb28e2741706cc40
mpierrejean <morgane.pierrejean@gmail.com>
2016-11-25 12:01:07 +0100

    adding information on used data set

---
+++ incas.extdata/R/data.R
@@ -0,0 +1,79 @@
+#' segment,GSE22928,MM,LAU108EBV
+#'
+#' dataset containing the output of PSCBS package
+#'
+#' @format A list with 5 components:
+#' \describe{
+#'   \item{data}{PairedPSCNData}
+#'   \item{output}{PairedPSCNSegments}
+#'   \item{tcnSegRows}{Segments of TCN}
+#'   \item{dhSegRows}{Segments of dh}
+#'   \item{params}{parameters for segmentation}
+#'   ...
+#' }
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE22928}
+"segment,GSE22928,MM,LAU108EBV"
+
+#' segment,GSE22928,MM,LAU108Me246
+#'
+#' dataset containing the output of PSCBS package
+#'
+#' @format A list with 5 components:
+#' \describe{
+#'   \item{data}{PairedPSCNData}
+#'   \item{output}{PairedPSCNSegments}
+#'   \item{tcnSegRows}{Segments of TCN}
+#'   \item{dhSegRows}{Segments of dh}
+#'   \item{params}{parameters for segmentation}
+#'   ...
+#' }
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE22928}
+"segment,GSE22928,MM,LAU108Me246"
+
+#' segment,GSE22928,MM,LAU108PBK
+#'
+#' dataset containing the output of PSCBS package
+#'
+#' @format A list with 5 components:
+#' \describe{
+#'   \item{data}{PairedPSCNData}
+#'   \item{output}{PairedPSCNSegments}
+#'   \item{tcnSegRows}{Segments of TCN}
+#'   \item{dhSegRows}{Segments of dh}
+#'   \item{params}{parameters for segmentation}
+#'   ...
+#' }
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE22928}
+"segment,GSE22928,MM,LAU108PBK"
+
+#' segment,GSE22928,MM,LAU149PBL
+#'
+#' dataset containing the output of PSCBS package
+#'
+#' @format A list with 5 components:
+#' \describe{
+#'   \item{data}{PairedPSCNData}
+#'   \item{output}{PairedPSCNSegments}
+#'   \item{tcnSegRows}{Segments of TCN}
+#'   \item{dhSegRows}{Segments of dh}
+#'   \item{params}{parameters for segmentation}
+#'   ...
+#' }
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE22928}
+"segment,GSE22928,MM,LAU149PBL"
+
+#' segment,GSE22928,MM,LAU149T149D
+#'
+#' dataset containing the output of PSCBS package
+#'
+#' @format A list with 5 components:
+#' \describe{
+#'   \item{data}{PairedPSCNData}
+#'   \item{output}{PairedPSCNSegments}
+#'   \item{tcnSegRows}{Segments of TCN}
+#'   \item{dhSegRows}{Segments of dh}
+#'   \item{params}{parameters for segmentation}
+#'   ...
+#' }
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE22928}
+"segment,GSE22928,MM,LAU149T149D"
